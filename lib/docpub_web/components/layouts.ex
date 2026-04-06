e5bf840be5aa0449371de470a91c109812e67624
andyl <andy@r210.com>
2026-04-06 16:58:42 -0700

    fix: update css again

--- lib/docpub_web/components/layouts.ex
+++ lib/docpub_web/components/layouts.ex
@@ -5,6 +5,9 @@ defmodule DocpubWeb.Layouts do
   """
   use DocpubWeb, :html
 
+  @asset_vsn :erlang.phash2(:os.system_time())
+  def asset_vsn, do: @asset_vsn
+
   embed_templates "layouts/*"
 
   @doc """
