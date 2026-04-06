fcbdf2e5aab8d93e6637fe69118f178fee4758ee
andyl <andy@r210.com>
2026-04-06 16:32:02 -0700

    chore: update icons

--- mix.exs
+++ mix.exs
@@ -81,7 +81,7 @@ defmodule Docpub.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:mdex, "~> 0.6"},
-      {:file_system, "~> 1.0"}
+      {:file_system, "~> 1.0"},
     ]
   end
 
