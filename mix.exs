d9b24e66ec5344c788a6235c0bffa425b9354cd7
andyl <andy@r210.com>
2026-04-09 12:01:49 -0700

    fix: add sysd

--- mix.exs
+++ mix.exs
@@ -63,6 +63,7 @@ defmodule Docpub.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
+      {:sysd, path: "~/src/Tool/sysd"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
